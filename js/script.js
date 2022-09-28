bcb7b050d5abbbb714c4498fb306b1f10362baf4
Matthew Payne <matthewpco86@gmail.com>
2022-09-27 18:50:15 -0700

    Added repo functionality
    Added function to fetch and display github repos for user

--- js/script.js
+++ js/script.js
@@ -2,31 +2,56 @@
 var overview = document.querySelector(".overview");
 //Github username
 var gitUserName = "Matthewpco";
+// Targeting list
+var repoList = document.querySelector(".repo-list");
 
-const getData = async function() {
+
+// Fetch Github user data
+const getUserData = async function() {
     const res = await fetch(
         `https://api.github.com/users/${gitUserName}`
         );
 
-        const data = await res.json();
-        console.log(data)
-        popData(data);
+        const uData = await res.json();
+        popData(uData);
 }
+getUserData();
 
-getData();
-console.log("end of line test");
-
-let popData = (data) => {
+// Populate fetched data 
+let popData = (uData) => {
    let dataBody =  document.createElement("div");
    dataBody.classList.add("user-info");
    dataBody.innerHTML = `<figure>
-   <img alt="user avatar" src=${data.avatar_url} />
+   <img alt="user avatar" src=${uData.avatar_url} />
  </figure>
  <div>
-   <p><strong>Name:</strong> ${data.name}</p>
-   <p><strong>Bio:</strong> ${data.bio}</p>
-   <p><strong>Location:</strong> ${data.location}</p>
-   <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+   <p><strong>Name:</strong> ${uData.name}</p>
+   <p><strong>Bio:</strong> ${uData.bio}</p>
+   <p><strong>Location:</strong> ${uData.location}</p>
+   <p><strong>Number of public repos:</strong> ${uData.public_repos}</p>
  </div> `;
 overview.append(dataBody)
+getRepoData();
 };
+
+
+// Fetch Github repo data
+const getRepoData = async function () {
+  const res = await fetch(
+    `https://api.github.com/users/${gitUserName}/repos?per_page=100&sort=updated`
+  );
+
+  const repoData = await res.json();
+  popRepo(repoData);
+};
+
+// Populate repo data
+
+const popRepo = (repoData) => {
+  for (const repo of repoData) {
+    const repoLi = document.createElement("li");
+    repoLi.classList.add(".repo");
+    repoLi.innerHTML = `<h3> ${repo.name} </h3>`;
+    repoList.append(repoLi);
+  }
+}
